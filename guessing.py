334ab663886e95b4dd433720c5b5e9685aca4159
Oluwanifemi Bamgbose <oluwanifemibamgbose@Oluwanifemis-MacBook-Pro.local>
2020-04-23 17:15:15 +0100

    Correction

--- guessing.py
+++ guessing.py
@@ -3,33 +3,31 @@ import random
 print("Welcome to the Guesser")
 print("Choose your Fate!")
 
-level = input("| For Easy - type 'E'| For Medium - type 'M' | For Hard - type 'H' |     ").upper()
-
 
 def create_guess():
     # Random number and tries generation based on level
+    level = input("| For Easy - type 'E'| For Medium - type 'M' | For Hard - type 'H' |     ").upper()
     if level == "E":
         correct_guess = random.randint(1, 10)
         tries = 6
-        return correct_guess, tries
     elif level == "M":
         correct_guess = random.randint(1, 20)
         tries = 4
-        return correct_guess, tries
     elif level == "H":
         correct_guess = random.randint(1, 50)
         tries = 3
-        return correct_guess, tries
+    return correct_guess, tries
 
 
 def check_guess():
     correct_guess, tries = create_guess()
     while tries > 0:
         # Accept user input
-        user_guess = input("Guess a number   ")
+        print(correct_guess)
+        user_guess = int(input("Guess a number   "))
         if user_guess == correct_guess:
             print("You got it right , awesome! ")
-            break
+            tries = 0
         elif user_guess != correct_guess:
             # Reduce the number of tries
             tries = tries - 1
@@ -44,10 +42,10 @@ def check_guess():
             else:
                 print("GAME OVER!")
                 # Restart or end game as user pleases
-                play_again = input("Would you like to Play again? (Y)es or (N)o     ")
-                if play_again == "Y":
-                    create_guess()
-                    check_guess()
+    play_again = input("Would you like to Play again? (Y)es or (N)o     ").upper()
+    if play_again == "Y":
+        create_guess()
+        check_guess()
 
 
 create_guess()
